756dfb515edf53810e26c2ff105281beb98139b8
DaniLee <162883213+DaniLeeTw@users.noreply.github.com>
2024-06-04 14:36:38 +0800

    trying

--- testsite/testsite.js
+++ testsite/testsite.js
@@ -1,3 +1,21 @@
+function toggleDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown button')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
 function checkAnswers() {
     // Define the correct answers
     const answers = {
@@ -30,3 +48,5 @@ function checkAnswers() {
         resultElement.style.color = "red";
     }
 }
+
+
