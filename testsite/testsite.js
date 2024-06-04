cfc5acec299d3ca0ce0dfedfa723c05bda0756a1
DaniLee <162883213+DaniLeeTw@users.noreply.github.com>
2024-06-04 17:01:41 +0800

    further developed the testing zone

--- testsite/testsite.js
+++ testsite/testsite.js
@@ -1,3 +1,21 @@
-const option = document.getElementsByNames("officialDocument")
-const optionContent = option.innerHTML;
-option.innerHTML = 
+const Petition = document.getElementById("Petition");
+const Letter = document.getElementById("Letter");
+const bothPAndL = document.getElementById("bothPAndL");
+const submit = document.getElementById("submit");
+const result = document.getElementById("result");
+//const paymentResult = document.getElementById("paymentResult");
+
+submit.onclick = function(){
+    if(Petition.checked){
+        result.textContent = `*還沒做好，請耐心*已選擇簽`;
+    }
+    else if(Letter.checked){
+        result.textContent = `*還沒做好，請耐心*已選擇函`;
+    }
+    else if(bothPAndL.checked){
+        result.textContent = `*還沒做好，請耐心*已選擇簽及函`;
+    }
+    else{
+        result.textContent = `請選擇公文類型！`;
+    }
+}
