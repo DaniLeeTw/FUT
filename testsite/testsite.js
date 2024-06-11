bd3d81af2c3269ad4b5c383f68b4c951b9cb077d
DaniLee <162883213+DaniLeeTw@users.noreply.github.com>
2024-06-11 17:05:08 +0800

    still working on the testsite

--- testsite/testsite.js
+++ testsite/testsite.js
@@ -1,34 +1,42 @@
-//claiming every id there is
-const myForm = document.getElementById("myForm");
-const title = document.getElementById("title");
-const generalConfig = document.getElementById("generalConfig");
-const chooseBetweenApproveAndAppropriate = document.getElementById("chooseBetweenApproveAndAppropriate");
+//selecting approve and showing approve types
 const approve = document.getElementById("approve");
 const appropriate = document.getElementById("appropriate");
 const preparedFund = document.getElementById("preparedFund");
 const selectApproveTypes = document.getElementById("selectApproveTypes");
+const selectAppropriateTypes = document.getElementById("selectAppropriateTypes");
 const approveType1 = document.getElementById("approveType1");
 const selectApproveType1Kinds = document.getElementById("selectApproveType1Kinds");
-const approveType1KindsControls = document.getElementById("approveType1KindsControls");
-const approveType1BothPAndLControls = document.getElementById("approveType1BothPAndLControls");
-const approveType1BothPAndL = document.getElementById("approveType1BothPAndL");
-const approveType1BothPAndLPContent = document.getElementById("approveType1BothPAndL-PContent");
-const approveType1BothPAndLPKeyInputs = document.getElementById("approveType1BothPAndLPKeyInputs");
-const approveType1BothPAndLPSchoolName = document.getElementById("approveType1BothPAndLPSchoolName");
-const approveType1BothPAndLPProjectName = document.getElementById("approveType1BothPAndLPProjectName");
-const approveType1BothPAndLPApproveAmount = document.getElementById("approveType1BothPAndLPApproveAmount");
-const approveType1BothPAndLPComingDate = document.getElementById("approveType1BothPAndLPComingDate");
-const approveType1BothPAndLPComingWord = document.getElementById("approveType1BothPAndLPComingWord");
-const approveType1BothPAndLPComingNumber = document.getElementById("approveType1BothPAndLPComingNumber");
-const approveType1BothPAndLPAlertDate = document.getElementById("approveType1BothPAndLPAlertDate");
-const approveType1BothPAndLPSituationArea = document.getElementById("approveType1BothPAndLPSituationArea");
-const approveType1BothPAndLPSituation = document.getElementById("approveType1BothPAndLPSituation");
-const approveType1BothPAndLPInvestigationDate = document.getElementById("approveType1BothPAndLPInvestigationDate");
-const approveType1BothPAndLPInvestigationResult = document.getElementById("approveType1BothPAndLPInvestigationResult");
-const approveType1BothPAndLPOriginalTotal = document.getElementById("approveType1BothPAndLPOriginalTotal");
+function selectApprove(){
+    if(approve.checked){
+        selectApproveTypes.style.display = "block";
+    }
+    if(appropriate.checked){
+        selectApproveTypes.style.display = "none";
+    }
+    if(preparedFund.checked){
+        selectApproveTypes.style.display = "none";
+    }
+}
+approve.addEventListener('change', selectApprove);
+appropriate.addEventListener('change', selectApprove);
+preparedFund.addEventListener('change', selectApprove);
 
+//selecting approve types and then showing the contents
 
-
+function selectAppropriate(){
+    if(appropriate.checked){
+        selectAppropriateTypes.style.display = "block";
+    }
+    if(approve.checked){
+        selectApproveTypes.style.display = "none";
+    }
+    if(preparedFund.checked){
+        selectApproveTypes.style.display = "none";
+    }
+}
+approve.addEventListener('change', selectApprove);
+appropriate.addEventListener('change', selectApprove);
+preparedFund.addEventListener('change', selectApprove);
 //yesSuggestedAmount.addEventListener('change', SuggestedAmount);
 //noSuggestedAmount.addEventListener('change', SuggestedAmount);
 
